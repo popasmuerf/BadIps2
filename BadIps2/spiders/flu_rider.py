e6315f134bec79ae90570a8c5a41d8c98f6f9df9
popasmuerf <MichaelBrown172@Gmail.com>
2019-01-23 23:19:24 -0500

    Refactor weekly parse to get the current month and  week number and then identify and download the appropiate weekly reports...

--- BadIps2/spiders/flu_rider.py
+++ BadIps2/spiders/flu_rider.py
@@ -17,7 +17,21 @@ class flu_rider(scrapy.Spider):
     lastYear = thisYear - 1
     thisYearStr = str(thisYear)
     lastYearStr = str(lastYear)
+    monthLst = ["null_month"
+                 "january",
+                 "February",
+                 "March",
+                 "April",
+                 "May",
+                 "June",
+                 "July",
+                 "August",
+                 "September",
+                 "October",
+                 "November",
+                 "December",]
     prevRunFlag = False
+
 
     name = 'rider'
     start_urls = ['https://www.gov.uk/government/collections/weekly-national-flu-reports']
@@ -52,14 +66,30 @@ class flu_rider(scrapy.Spider):
                     yield scrapy.Request(url=_url, callback=self.parseAllSeasonalReports)
 
 
-    def getMoth(self):
+    def getMonth(self):
         #create month map
         #create
+        pass
 
     def parsSeasonalReports(self,response):
         #get link to weekly reports
         #follow weekly reports path
-        pass
+        urllist = response.xpath('//*[@id="content"]/div[3]/div[2]/div/div/nav/ul/li[1]/a/@href').extract()
+        #check if urlist is empty
+        if not urllist:
+            exit(-1)
+        else:
+            _url = urllist[1]
+            result = re.findall('https{0,1}://',_url)
+            if result:
+                _url = 'https://www.gov.uk/government/collections/weekly-national-flu-reports'
+                #call parseWeeklyReports
+                pass
+            else:
+                _url =  'https://www.gov.uk' + _url
+                #call parseWeeklyReports
+                pass
+
 
     def parseWeeklyReports(selfs,response):
         #get the current month
@@ -76,17 +106,37 @@ class flu_rider(scrapy.Spider):
             if result:
                 print _url
 
+        else:
+            _url = self.reportUrlPrefix + _url
+            print _url
+
+
+
+def parseWeeklyReports(self,response):
+    urllist = response.xpath('//*[@id="contents"]/div[2]/div/ol/li/a/@href').extract()
+    if not urllist:
+        exit(-1)
+    else:
+        for _url in urllist:
+            if _url.find(self.thisYearStr) == -1:
+                pass
             else:
-                _url = self.reportUrlPrefix + _url
-                print _url
-        pass
+                result = re.findall('https{0,1}://',_url)
+                if result:
+                    #follw path and download all files
+                    pass
+                else:
+                    _url = self.reportUrlPrefix + _url
+                    #follow path and download all files
 
 
-    def parseWeeklyReports(self,response):
-        pass
 
 
 
 
-    def downloadReport(self):
-        pass
+
+def downloadAllReports(self,response):
+    monthInt = self.now.month
+    monthStr = self.monthLst[monthInt]
+
+    pass
